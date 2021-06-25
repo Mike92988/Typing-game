08e008c3425c9b231fe5287bbcb764768bece722
Mike92988 <79021524+Mike92988@users.noreply.github.com>
2021-06-25 15:29:25 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,126 @@
+const word = document.getElementById('word');
+const text = document.getElementById('text');
+const scoreEl = document.getElementById('score');
+const timeEl = document.getElementById('time');
+const endgameEl = document.getElementById('end-game-container');
+const settingsBtn = document.getElementById('settings-btn');
+const settings = document.getElementById('settings');
+const settingsForm = document.getElementById('settings-form');
+const difficultySelect = document.getElementById('difficulty');
+
+
+
+const words = [
+    'sigh',
+    'tense',
+    'airplane',
+    'ball',
+    'pies',
+    'juice',
+    'warlike',
+    'bad',
+    'north',
+    'dependent',
+    'steer',
+    'silver',
+    'highfalutin',
+    'superficial',
+    'quince',
+    'eight',
+    'feeble',
+    'admit',
+    'drag',
+    'loving',
+    'banana',
+    'apple',
+    'potato',
+    'ham',
+    'zebra',
+    'hoagie',
+    'pizza'
+  ];
+  
+let randomWord;
+
+let score = 0
+
+let time = 10;
+//difficulty set to value in local storage
+let difficulty = localStorage.getItem('difficulty') !== null ? localStorage.getItem('difficulty') : 'medium';
+
+difficultySelect.value = difficulty
+
+//focus on text on start
+text.focus()
+
+const timeInterval = setInterval(updateTime, 1000)
+//generat random word
+function getRandomWord() {
+    return words[Math.floor(Math.random() * words.length)]
+}
+
+
+//add word to dom 
+
+function addWordToDOM() {
+    randomWord = getRandomWord()
+    word.innerHTML = randomWord;
+}
+
+function updateScore() {
+    score++;
+    scoreEl.innerHTML = score
+}
+
+function updateTime() {
+   time--;
+   timeEl.innerHTML = time + 's'
+   if(time === 0) {
+       clearInterval(timeInterval);
+       gameOver()
+   }
+}
+
+function gameOver() {
+    endgameEl.innerHTML =`
+    <h1>Time Ran Out</h1>
+    <p>Your final score is ${score}</p>
+    <button onclick="location.reload()">Reload</button>
+    `;
+
+    endgameEl.style.display = 'flex'
+}
+addWordToDOM();
+
+text.addEventListener('input', e => {
+    const insertedText = e.target.value
+    if(insertedText === randomWord) {
+        addWordToDOM()
+        updateScore()
+
+    // clear
+        e.target.value = ''
+
+        if(difficulty === 'hard') {
+            time += 2
+        } else if(difficulty === 'medium') {
+            time += 3
+        } else {
+            time += 5
+        }
+
+        
+        updateTime()
+    }
+})
+
+
+//settings
+settingsBtn.addEventListener('click', () => settings.classList.toggle('hide'));
+
+//difficulty select
+
+settingsForm.addEventListener('change', e => {
+    difficulty = e.target.value;
+    localStorage.setItem('difficulty', difficulty);
+})
